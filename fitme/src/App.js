eb83a930e49277b29c01500d49ffb487e9c96548
Sajay0623 <sajay9885@gmail.com>
2022-12-14 23:50:26 +0530

    carousal

---
+++ fitme/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Carousel from './Component/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Carousel />
+    </div>
+  );
+}
+
+export default App;
